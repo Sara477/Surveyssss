92f474995a698155442b6ba67575f455bc865256
Anastasija <ancianastasijab@gmail.com>
2023-09-17 09:48:09 +0200

    dodati backend deo - controleri request recources

--- aplikacija/routes/api.php
+++ aplikacija/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,9 @@ use Illuminate\Support\Facades\Route;
 */
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    //Route::get('/me', [AuthController::class, 'me']);
-    //Route::apiResource('survey', SurveyController::class);
+
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('survey', SurveyController::class);
 
     //Route::get('/dashboard', [DashboardController::class, 'index']);
 });
